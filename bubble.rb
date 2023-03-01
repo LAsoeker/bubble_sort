5e464eab8120a0f0d0db82db55009cdfea2c3c26
msoeker <80034462+msoeker@users.noreply.github.com>
2023-03-01 13:51:10 +0100

    bubble sort first commit

---
+++ bubble.rb
@@ -0,0 +1,21 @@
+data = [4,3,78,2,0,2]
+
+def bubble_sort(arr)
+  arr.each_with_index do |val, index|
+    swapped = false
+    arr.each_with_index do |val_inner, index_inner|
+      if arr[index_inner+1] == nil
+        break
+      elsif arr[index_inner]>arr[index_inner+1]
+        arr[index_inner], arr[index_inner+1] = arr[index_inner+1], arr[index_inner] 
+        swapped = true
+      end
+    end
+    unless swapped
+      return arr
+    end
+  end  
+
+end
+
+p bubble_sort(data)
